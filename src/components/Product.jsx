d8b07351567fcce34ce30c3ccfb842cfb242e3e1
raghul pasupathi <raghulpasupathi@gmail.com>
2023-05-19 21:29:47 +0530

    ecommerce site home page css and data props

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
-import '../styles/Product.css'
+import React from "react";
+import "../styles/Product.css";
 
-function Product() {
+function Product({ id, title, price, rating, image }) {
   return (
-    <div className='product'>
-        <div className="product__info">
-            <p>Title</p>
-            <p className="product__price">$50</p>
-            <div className="product__rating">⭐⭐⭐⭐</div>
-
-            <img src="https://m.media-amazon.com/images/I/71ummMzf9yL._SL1500_.jpg" alt="product__image" />
-
-            <button>Add to cart</button>
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map(() => (
+              <p>⭐</p>
+            ))}
         </div>
+      </div>
+
+      <img src={image} alt="product__image" />
+
+      <button>Add to cart</button>
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
